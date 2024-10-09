639b29d656ade8ef68d5f8a4eff4969be2a4d16f
Savindu Amalka <savinduamalka@gmail.com>
2024-10-09 23:58:23 +0530

    authentication middleware created

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 import bodyParser from 'body-parser';
 import express from 'express'
-import { connect } from 'http2';
 import mongoose from 'mongoose';
 import userRouter from './routes/userRouter.js';
 import galleryItemsRoute from './routes/galleryItemsRoute.js';
+import jwt from 'jsonwebtoken';
 
 
 const app = express();
 app.use(bodyParser.json());
+const connection ="mongodb+srv://user1:123@cluster0.lynjp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+//Authentication middleware
+app.use((req,res,next)=>{
+    const token =req.header("Authorization")?.replace("Bearer ","");
+    
+    if(token!=null){
+        jwt.verify(token,"secret",(error,decoded)=>{
+        if(decoded!=null){
+        req.body.user=decoded;
+        next();
+        }else{
+            next();
+        }
+    })
+    }else{
+        next();
+    }
+});
 app.use("/api/users",userRouter);
 app.use("/api/gallery",galleryItemsRoute);
 
-const connection ="mongodb+srv://user1:123@cluster0.lynjp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 
 mongoose.connect(connection).then(
     ()=>{
