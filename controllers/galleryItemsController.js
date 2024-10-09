b6e8cce76192aefd9ed8fa36ed2e9ad7ba71f4b9
Savindu Amalka <savinduamalka@gmail.com>
2024-10-09 23:59:37 +0530

    add gallery handle with user login

--- controllers/galleryItemsController.js
+++ controllers/galleryItemsController.js
@@ -1,41 +1,46 @@
 import GalleryItems from "../models/galleryItems.js";
 
-
-export function getGalleryItems(req,res){
-    const galleryItem=req.body;
-    GalleryItems.find().then(
-        (list)=>{
-            res.json({
-                galleryItems:list
-            });
-        }
-    ).catch(
-        (error)=>{
-            res.json({
-                message:"Gallery Item can't be retrieved",
-                error:error.message
-            })
-        }
-    )
+export function getGalleryItems(req, res) {
+  const galleryItem = req.body;
+  GalleryItems.find()
+    .then((list) => {
+      res.json({
+        galleryItems: list,
+      });
+    })
+    .catch((error) => {
+      res.json({
+        message: "Gallery Item can't be retrieved",
+        error: error.message,
+      });
+    });
 }
 
-export function createGalleryItems(req,res){
-    const galleryItem=req.body;
-    
-    const newGalleryItem = new GalleryItems(galleryItem); 
+export function createGalleryItems(req, res) {
+  const user = req.body.user;
+  if (user == null) {
+     res.status(403).json({
+      message: "Please login to create gallery item",
+    });
+    return;
+  }
+  
 
-    newGalleryItem.save().then(
-        ()=>{
-            res.json({
-                message:"Gallery Item created successfully"
-            });
-        }
-    ).catch(
-        (error)=>{
-            res.status(400).json({
-                message:"Gallery Item can't be created",
-                error:error.message
-            });
-        }
-    );
+  const galleryItem = req.body.item;
+
+  const newGalleryItem = new GalleryItems(galleryItem);
+
+  newGalleryItem
+    .save()
+    .then(() => {
+      res.json({
+        message: "Gallery Item created successfully",
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        message: "Gallery Item can't be created",
+        error: error.message,
+      });
+    });
 }
