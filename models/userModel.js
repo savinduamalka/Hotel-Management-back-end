8344e13dffad89e62af1030acb6efa6419834b35
Savindu Amalka <savinduamalka@gmail.com>
2024-10-11 09:20:11 +0530

    user model add password attribute

--- models/userModel.js
+++ models/userModel.js
@@ -6,6 +6,10 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true    
     },
+    password:{
+        type: String,
+        required: true
+    },
     firstname: {
         type: String,
         required: true  
