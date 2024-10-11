c5e334b334b0b7b9c03edde79be20c7bbfe3d93b
Savindu Amalka <savinduamalka@gmail.com>
2024-10-11 16:36:40 +0530

    "created category schema"

---
+++ models/category.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const categorySchema = mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        description:{
+            type:String,
+            required:true
+        },
+        price:{
+            type:Number,
+            required:true
+        }
+    }
+);
+
+const Category=mongoose.model("categories",categorySchema);
+
+export default Category;
