fb58908f9703caec6be41e60cac96f7f85954341
Savindu Amalka <savinduamalka@gmail.com>
2024-10-13 22:59:21 +0530

    category crud completed

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,5 @@
 import Category from '../models/category.js';
+import { checkAdmin } from './userController.js';
 
 export function getCategories(req, res) {
     Category.find()
@@ -16,23 +17,14 @@ export function getCategories(req, res) {
 };
 
 export function createCategory(req, res) {
-    const user = req.user;
-    if (!user) {
+    if(!checkAdmin(req)){
         res.status(403).json({
-            message: "Please login to create category",
-        });
-        return;
-    }
-    if (user.payloader.type != 'admin') { 
-        res.status(403).json({
-            message: "You are not allowed to create category",
+            message: "You are not allowed to create Category",
         });
         return;
     }
 
-    const categoryData = req.body;
-
-    const newCategory = new Category(categoryData);
+    const newCategory = new Category(req.body);
 
     newCategory.save()
         .then((result) => {
@@ -48,3 +40,68 @@ export function createCategory(req, res) {
             });
         });
 };
+
+export function updateCategoryByName(req, res) {
+    if(!checkAdmin(req)){
+        res.status(403).json({
+            message: "You are not allowed to update Category",
+        });
+        return;
+    }
+
+    const categoryName = req.params.name;
+    const category = req.body;
+
+    Category.findOneAndUpdate({name:categoryName}, category, {new:true})
+        .then((result) => {
+            res.json({
+                message: "Category updated successfully",
+                result: result,
+            });
+        })
+        .catch((error) => {
+            res.status(400).json({
+                message: "Category can't be updated",
+                error: error.message,
+            });
+        });
+}
+
+export function getCategoryByName(req,res){
+    Category.findOne({name:req.params.name})
+    .then(
+        (result)=>{
+            res.json({
+                category:result
+            })
+        }
+    ).catch(
+        (err)=>{
+            res.status(500).json({
+                error:err.message
+            })
+        }
+    )
+}
+
+export function deleteCategoryByName(req,res){
+    if(!checkAdmin(req)){
+        res.status(403).json({
+            message: "You are not allowed to delete Category",
+        });
+    }
+    Category.findOneAndDelete({name:req.params.name})
+    .then(
+        ()=>{
+            res.json({
+                message:"Category deleted successfully"
+            })
+        }
+    ).catch(
+        (err)=>{
+            res.status(500).json({
+                error:err.message
+            })
+        }
+    )
+}
--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,10 +1,14 @@
 import express from "express";
-import {createCategory, getCategories} from "../controllers/categoryController.js";
+import {createCategory, deleteCategoryByName, getCategories, getCategoryByName, updateCategoryByName} from "../controllers/categoryController.js";
+
 
 const categoryRouter = express.Router();
 
 categoryRouter.get("/", getCategories);
 categoryRouter.post("/", createCategory);
+categoryRouter.put("/:name", updateCategoryByName);
+categoryRouter.get("/:name", getCategoryByName);
+categoryRouter.delete("/:name", deleteCategoryByName);
 
 
 export default categoryRouter;
