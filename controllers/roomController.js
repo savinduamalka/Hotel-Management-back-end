3601f23a0606137e83896b3d681abbb87ce1e552
Savindu Amalka <savinduamalka@gmail.com>
2024-10-14 09:53:34 +0530

    get room function implemented

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -11,19 +11,34 @@ export function createRoom(req, res) {
 
   const newRoom = new Room(req.body);
 
-  newRoom.save().then(
-    (result)=>{
-        res.json({
-            message: "Room Created Successfully",
-            result: result
-        })
-    }
-  ).catch(
-    (error)=>{
-        res.status(500).json({
-            message: "Can not create a Room",
-            error:error
-        })
+  newRoom
+    .save()
+    .then((result) => {
+      res.json({
+        message: "Room Created Successfully",
+        result: result,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Can not create a Room",
+        error: error,
+      });
+    });
+}
+
+export function getRoom(req, res) {
+  Room.find().then(
+    (result) => {
+    res.json({
+      list: result,
+    });
+  }).catch(
+    (err)=>{
+      res.status(500).json({
+        message: "Failed to retrieve rooms",
+        err:err
+      })
     }
   )
 }
--- routes/roomRoute.js
+++ routes/roomRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createRoom } from '../controllers/roomController.js';
+import { createRoom, getRoom } from '../controllers/roomController.js';
 
 const roomRouter = express.Router();
 
 roomRouter.post("/",createRoom);
+roomRouter.get("/",getRoom);
 
 export default roomRouter;
