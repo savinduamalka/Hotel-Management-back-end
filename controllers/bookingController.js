589146290e5e5362c59409b419f7eaaf9941cee9
Savindu Amalka <savinduamalka@gmail.com>
2024-10-15 12:15:21 +0530

    create booking implemented

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,7 +1,15 @@
 import Booking from '../models/bookingModel.js';
+import { checkCustomer } from './userController.js';
 
 
 export function createBooking(req, res) {
+    if(!checkCustomer(req)){
+        console.log(req.user.type);
+        res.status(403).json({
+            message: "Please login as a customer to continue the booking"
+        });
+        return;
+    }
     const starting = 1000;
 
     Booking.countDocuments({})
@@ -10,5 +18,25 @@ export function createBooking(req, res) {
             console.log(bookingId);
             console.log(countDocuments);
            
+            const newBooking = new Booking({
+                bookingId,
+                email: req.user.email,
+                roomId: req.body.roomId,
+                startDate: req.body.startDate,
+                endDate: req.body.endDate
+            });
+            return newBooking.save();
         })
+        .then((result) => {
+            res.json({
+                message: "Booking created successfully",
+                result: result
+            });
+        })
+        .catch((error) => {
+            res.json({
+                message: "Booking failed",
+                error: error
+            });
+        }).catch();
 }
