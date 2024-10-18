dfee317f32dfece24166204fdb6d55b2b3312a50
Savindu Amalka <savinduamalka@gmail.com>
2024-10-18 23:32:46 +0530

    update feedback visibility route set

--- routes/feedbackRouter.js
+++ routes/feedbackRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createFeedback } from '../controllers/feedbackController.js';
+import { createFeedback,  updateFeedbackVisibility } from '../controllers/feedbackController.js';
 
 const feedbackRouter = express.Router();
 
 feedbackRouter.post("/", createFeedback);
+feedbackRouter.put("/:feedbackId", updateFeedbackVisibility);
 
 
 
