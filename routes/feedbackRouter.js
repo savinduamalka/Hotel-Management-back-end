2a5a5428df57b439be08fd39f101eb3e474633aa
Savindu Amalka <savinduamalka@gmail.com>
2024-10-20 23:47:55 +0530

    delete feedback route set

--- routes/feedbackRouter.js
+++ routes/feedbackRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createFeedback,  getFeedback,  updateFeedbackVisibility } from '../controllers/feedbackController.js';
+import { createFeedback,  deleteFeedback,  getFeedback,  updateFeedbackVisibility } from '../controllers/feedbackController.js';
 
 const feedbackRouter = express.Router();
 
 feedbackRouter.post("/", createFeedback);
 feedbackRouter.put("/:feedbackId", updateFeedbackVisibility);
 feedbackRouter.get("/",getFeedback);
+feedbackRouter.delete("/:feedbackId",deleteFeedback);
 
 
 
