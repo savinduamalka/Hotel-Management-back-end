0241504f9b46e1e5b52e5f326f4d844569bab3cf
Savindu Amalka <savinduamalka@gmail.com>
2025-09-24 21:54:35 +0530

    add publicInquiry route to inquiryRouter for handling public inquiries

--- routes/inquiryRouter.js
+++ routes/inquiryRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createInquiry, deleteInquiry, getInquiry, updateInquiryReply } from '../controllers/inquiryController.js';
+import { createInquiry, deleteInquiry, getInquiry, publicInquiry, updateInquiryReply } from '../controllers/inquiryController.js';
 
 
 
@@ -9,5 +9,6 @@ inqiuiryRouter.post("/", (createInquiry));
 inqiuiryRouter.get("/", getInquiry);
 inqiuiryRouter.put("/:inquiryId", updateInquiryReply);
 inqiuiryRouter.delete("/:email", deleteInquiry);
+inqiuiryRouter.post("/public-inquiry", publicInquiry);
 
 export default inqiuiryRouter;
