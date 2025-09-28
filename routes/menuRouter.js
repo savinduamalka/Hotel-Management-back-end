7bab6f253cbd10f4b1998a3ba48b012c9613f6af
Savindu Amalka <savinduamalka@gmail.com>
2025-09-28 13:58:38 +0530

    add getAllMenu and getHotMenu routes to menuRouter for menu retrieval

--- routes/menuRouter.js
+++ routes/menuRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { addMenuData } from '../controllers/menuController.js';
+import { addMenuData, getAllMenu, getHotMenu } from '../controllers/menuController.js';
 
 const menuRouter = express.Router();
 
-
+// Admin only bulk upsert
 menuRouter.post('/bulk', addMenuData);
-
+// Public
+menuRouter.get('/', getAllMenu);
+menuRouter.get('/hot', getHotMenu);
 
 export default menuRouter;
